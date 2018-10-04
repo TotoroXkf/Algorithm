ef9e3db353ae6240e8be17fccce08c0cda1dcb91
TotoroXkf <1131225133@qq.com>
2018-10-04 22:43:09 +0800

    note

--- solution.py
+++ solution.py
@@ -1,30 +1,19 @@
 from node import ListNode, create_linked_list, print_linked_list
 
 
-def add_two_numbers(l1: ListNode, l2: ListNode):
-    x, y = l1, l2
-    while x is not None and y is not None:
-        value = x.val+y.val
-        x.val, y.val = value, value
-        x, y = x.next, y.next
-    y, x = x, l1
-    if x is None:
-        y, x = x, l2
-    n = 0
-    while x is not None:
-        x.val += n
-        if x.val >= 10:
-            n = 1
-            x.val -= 10
-            if x.next is None:
-                x.next = ListNode(1)
-                break
-        else:
-            n = 0
-        x = x.next
-    return y
+def merge_two_link_list(l1: ListNode, l2: ListNode):
+    if l1 is None:
+        return l2
+    if l2 is None:
+        return l1
+    if l1.val < l2.val:
+        l1.next = merge_two_link_list(l1.next, l2)
+        return l1
+    else:
+        l2.next = merge_two_link_list(l1, l2.next)
+        return l2
 
 
-l1 = create_linked_list([1, 2, 3, 4, 5])
-l2 = create_linked_list([2, 3, 4, 5, 6])
-print_linked_list(add_two_numbers(l1, l2))
+l1 = create_linked_list([1, 3, 5, 7, 9])
+l2 = create_linked_list([2, 4, 6, 8, 10])
+print_linked_list(merge_two_link_list(l1,l2))
