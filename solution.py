694b9544bddffc9f9493967cfeaac9c069837206
TotoroXkf <1131225133@qq.com>
2018-10-04 22:11:56 +0800

    note

--- solution.py
+++ solution.py
@@ -1,31 +1,27 @@
-def get_max_len_common(str1: str, str2: str):
-    if len(str1) == 0 or len(str2) == 0:
-        return ""
-
-    def get_dp():
-        dp = [[0 for i in range(len(str2)+1)] for j in range(len(str1)+1)]
-        for i in range(1, len(dp)):
-            for j in range(1, len(dp[0])):
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-                if str1[i-1] == str2[j-1]:
-                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
-        return dp
-
-    dp = get_dp()
-    i, j = len(dp)-1, len(dp[0])-1
-    res = ['' for i in range(dp[-1][-1])]
-    index = len(res)-1
-    while dp[i][j] != 0:
-        if dp[i][j] == dp[i-1][j]:
-            i -= 1
-        elif dp[i][j] == dp[i][j - 1]:
-            j -= 1
-        else:
-            res[index] = str1[i-1]
-            index, i, j = index-1, i-1, j-1
-    return "".join(res)
+def min_path_sum(matrix: list):
+    more = max(len(matrix), len(matrix[0]))
+    less = min(len(matrix), len(matrix[0]))
+    row_more = more == len(matrix)
+    dp = [0 for i in range(less)]
+    dp[0] = matrix[0][0]
+    for i in range(1, less):
+        dp[i] = dp[i - 1] + (matrix[0][i] if row_more else matrix[i][0])
+    for i in range(1, more):
+        dp[0] += matrix[i][0] if row_more else matrix[0][i]
+        for j in range(1, less):
+            value = matrix[i][j] if row_more else matrix[j][i]
+            dp[j] = min(dp[j - 1], dp[j]) + value
+    return dp[-1]
 
 
-str1 = "1A2C3D4B56"
-str2 = "B1D23CA45B6A"
-print(get_max_len_common(str1, str2))
+test = [
+    [1, 2, 3, 1],
+    [1, 2, 2, 2],
+    [2, 5, 7, 1]
+]
+print(min_path_sum(test))
+result = [
+    [1, 3, 6, 7],
+    [2, 4, 6, 8],
+    [3, 8, 13, 9]
+]
