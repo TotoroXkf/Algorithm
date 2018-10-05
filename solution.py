5da2b0e843e656718db9f060484e8f89eb97122a
TotoroXkf <1131225133@qq.com>
2018-10-05 08:14:47 +0800

    note

--- solution.py
+++ solution.py
@@ -1,19 +1,10 @@
-from node import ListNode, create_linked_list, print_linked_list
-
-
-def merge_two_link_list(l1: ListNode, l2: ListNode):
-    if l1 is None:
-        return l2
-    if l2 is None:
-        return l1
-    if l1.val < l2.val:
-        l1.next = merge_two_link_list(l1.next, l2)
-        return l1
-    else:
-        l2.next = merge_two_link_list(l1, l2.next)
-        return l2
-
-
-l1 = create_linked_list([1, 3, 5, 7, 9])
-l2 = create_linked_list([2, 4, 6, 8, 10])
-print_linked_list(merge_two_link_list(l1,l2))
+def length_of_longest_sub_string(s: str):
+    n, j, i, ans = len(s), 0, 0, 0
+    position_map = {}
+    while j < n:
+        if position_map.__contains__(s[j]):
+            i = max(position_map[s[j]], i)
+        ans = max(ans, j - i + 1)
+        position_map[s[j]] = j + 1
+        j += 1
+    return ans
