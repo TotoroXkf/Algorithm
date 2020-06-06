41339f0ac48158575d4c6bd0f51b04bd09a9be52
xiakaifa <xkf123456789@gmail.com>
2020-06-06 16:44:04 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,14 @@
 class Solution {
-    public void recoverTree(TreeNode root) {
-        TreeNode[] preNode = new TreeNode[1];
-        TreeNode[] result = new TreeNode[2];
-        getNodes(root, result, preNode);
-        int temp = result[0].val;
-        result[0].val = result[1].val;
-        result[1].val = temp;
-    }
-
-    public void getNodes(TreeNode root, TreeNode[] result, TreeNode[] preNode) {
-        if (root == null) {
-            return;
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
         }
-        getNodes(root.left, result, preNode);
-        if (preNode[0] != null && preNode[0].val > root.val) {
-            if (result[0] == null) {
-                result[0] = preNode[0];
-            }
-            result[1] = root;
+        if (p == q) {
+            return true;
         }
-        preNode[0] = root;
-        getNodes(root.right, result, preNode);
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
     }
 }
