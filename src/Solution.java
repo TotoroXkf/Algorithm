d126c2ec69c8d2ba00e3f47da776272bb5023406
xiakaifa <xkf123456789@gmail.com>
2020-06-10 09:16:14 +0800

    二叉树的最大深度

--- src/Solution.java
+++ src/Solution.java
@@ -1,41 +1,10 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
+    public int maxDepth(TreeNode root) {
         if (root == null) {
-            return result;
+            return 0;
         }
-        LinkedList<Integer> currentList = new LinkedList<>();
-        int level = 1;
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        queue.add(null);
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.removeFirst();
-            if (node == null) {
-                if (!queue.isEmpty()) {
-                    queue.add(null);
-                    level++;
-                }
-                result.add(currentList);
-                currentList = new LinkedList<>();
-                continue;
-            }
-            if (level % 2 == 0) {
-                currentList.add(0, node.val);
-            } else {
-                currentList.add(node.val);
-            }
-            if (node.left != null) {
-                queue.add(node.left);
-            }
-            if (node.right != null) {
-                queue.add(node.right);
-            }
-        }
-        return result;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 }
