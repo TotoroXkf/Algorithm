beb03b477160fe062784352436166f0926a7656f
xiakaifa <xkf123456789@gmail.com>
2020-06-11 09:23:00 +0800

    从中序和后序恢复二叉树

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,24 @@
 class Solution {
-    public TreeNode buildTree(int[] preOrder, int[] inOrder) {
-        return buildTree(preOrder, 0, preOrder.length, inOrder, 0, inOrder.length);
+    public TreeNode buildTree(int[] inOrder, int[] postOrder) {
+        return buildTree(inOrder, 0, inOrder.length, postOrder, 0, postOrder.length);
     }
 
-    private TreeNode buildTree(int[] preOrder, int preLeft, int preRight, int[] inOrder, int inLeft, int inRight) {
-        if (preRight - preLeft == 0 || inRight - inLeft == 0) {
+    private TreeNode buildTree(int[] inOrder, int inStart, int inEnd, int[] postOrder, int postStart, int postEnd) {
+        if (inEnd - inStart == 0 || postEnd - postStart == 0) {
             return null;
         }
-        if (preRight - preLeft == 1 || inRight - inLeft == 1) {
-            return new TreeNode(preOrder[preLeft]);
+        TreeNode root = new TreeNode(postOrder[postEnd - 1]);
+        if (inEnd - inStart == 1 || postEnd - postStart == 1) {
+            return root;
         }
-        int preIndex = preLeft;
-        int inIndex = inLeft;
-        while (inOrder[inIndex] != preOrder[preLeft]) {
+        int inIndex = inStart;
+        int postIndex = postStart;
+        while (inOrder[inIndex] != postOrder[postEnd - 1]) {
             inIndex++;
-            preIndex++;
+            postIndex++;
         }
-        TreeNode root = new TreeNode(preOrder[preLeft]);
-        TreeNode leftNode = buildTree(preOrder, preLeft + 1, preIndex + 1, inOrder, inLeft, inIndex);
-        TreeNode rightNode = buildTree(preOrder, preIndex + 1, preRight, inOrder, inIndex + 1, inRight);
-        root.left = leftNode;
-        root.right = rightNode;
+        root.left = buildTree(inOrder, inStart, inIndex, postOrder, postStart, postIndex);
+        root.right = buildTree(inOrder, inIndex + 1, inEnd, postOrder, postIndex, postEnd - 1);
         return root;
     }
 }
