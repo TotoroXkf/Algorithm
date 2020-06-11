983e0b78c7785debe70c765192e6090d9c10f66c
xiakaifa <xkf123456789@gmail.com>
2020-06-11 09:09:50 +0800

    从中序和前序恢复二叉树

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,26 @@
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
+    public TreeNode buildTree(int[] preOrder, int[] inOrder) {
+        return buildTree(preOrder, 0, preOrder.length, inOrder, 0, inOrder.length);
+    }
+
+    private TreeNode buildTree(int[] preOrder, int preLeft, int preRight, int[] inOrder, int inLeft, int inRight) {
+        if (preRight - preLeft == 0 || inRight - inLeft == 0) {
+            return null;
         }
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
-        return Math.max(leftDepth, rightDepth) + 1;
+        if (preRight - preLeft == 1 || inRight - inLeft == 1) {
+            return new TreeNode(preOrder[preLeft]);
+        }
+        int preIndex = preLeft;
+        int inIndex = inLeft;
+        while (inOrder[inIndex] != preOrder[preLeft]) {
+            inIndex++;
+            preIndex++;
+        }
+        TreeNode root = new TreeNode(preOrder[preLeft]);
+        TreeNode leftNode = buildTree(preOrder, preLeft + 1, preIndex + 1, inOrder, inLeft, inIndex);
+        TreeNode rightNode = buildTree(preOrder, preIndex + 1, preRight, inOrder, inIndex + 1, inRight);
+        root.left = leftNode;
+        root.right = rightNode;
+        return root;
     }
 }
