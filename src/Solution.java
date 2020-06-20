e09400c465c977dd1a83eed73c994fbc6db6764b
xiakaifa <xkf123456789@gmail.com>
2020-06-20 22:14:31 +0800

    路径总和

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> currentList = new LinkedList<>();
+        pathSum(root, sum, currentList, result);
+        return result;
+    }
+
+    private void pathSum(TreeNode root, int sum, List<Integer> currentList, List<List<Integer>> result) {
         if (root == null) {
-            return false;
+            return;
         }
         sum -= root.val;
+        currentList.add(root.val);
         if (sum == 0 && root.left == null && root.right == null) {
-            return true;
+            result.add(new ArrayList<>(currentList));
+            currentList.remove(currentList.size() - 1);
+            return;
         }
-        boolean leftResult = hasPathSum(root.left, sum);
-        if (!leftResult) {
-            return hasPathSum(root.right, sum);
-        }
-        return true;
+        pathSum(root.left, sum, currentList, result);
+        pathSum(root.right, sum, currentList, result);
+        currentList.remove(currentList.size() - 1);
     }
 }
