f98da7dbb603cfa3ff0a68945bc47a6e5f099b70
xiakaifa <xkf123456789@gmail.com>
2020-06-20 21:51:40 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,16 @@
 class Solution {
-    public int minDepth(TreeNode root) {
+    public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) {
-            return 0;
+            return false;
         }
-        int leftDepth = minDepth(root.left);
-        int rightDepth = minDepth(root.right);
-        if (leftDepth > 0 && rightDepth > 0) {
-            return 1 + Math.min(leftDepth, rightDepth);
-        } else if (leftDepth > 0) {
-            return 1 + leftDepth;
-        } else if (rightDepth > 0) {
-            return 1 + rightDepth;
-        } else {
-            return 1;
+        sum -= root.val;
+        if (sum == 0 && root.left == null && root.right == null) {
+            return true;
         }
+        boolean leftResult = hasPathSum(root.left, sum);
+        if (!leftResult) {
+            return hasPathSum(root.right, sum);
+        }
+        return true;
     }
 }
