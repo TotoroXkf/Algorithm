3b4123ad8701baa2eff5af4d8c2881ddea1b5159
xiakaifa <xkf123456789@gmail.com>
2020-06-27 14:11:05 +0800

    买卖股票的最佳时机2

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -6,14 +6,12 @@ public class Solution {
         if (prices.length == 0) {
             return result;
         }
-        int minValue = prices[0];
+        int preValue = prices[0];
         for (int i = 1; i < prices.length; i++) {
-            if (minValue > prices[i]) {
-                minValue = prices[i];
-            } else {
-                int earn = prices[i] - minValue;
-                result = Math.max(earn, result);
+            if (preValue <= prices[i]) {
+                result += prices[i] - preValue;
             }
+            preValue = prices[i];
         }
         return result;
     }
