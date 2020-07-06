d73e6d9c13031506b1202b099ed50ae85526fac9
xiakaifa <xkf123456789@gmail.com>
2020-07-06 08:46:33 +0800

    Coding

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,30 +1,25 @@
 package main;
 
+import struct.TreeNode;
+
 public class Solution {
-    public int maxProfit(int[] prices) {
-        int[] left = new int[prices.length];
-        int max = 0;
-        int leftMin = Integer.MAX_VALUE;
-        for (int i = 0; i < prices.length; i++) {
-            leftMin = Math.min(prices[i], leftMin);
-            max = Math.max(prices[i] - leftMin, max);
-            left[i] = max;
-        }
+    private int max = Integer.MIN_VALUE;
 
-        max = 0;
-        int[] right = new int[prices.length];
-        int rightMax = Integer.MIN_VALUE;
-        for (int i = prices.length - 1; i > -1; i--) {
-            rightMax = Math.max(rightMax, prices[i]);
-            max = Math.max(rightMax - prices[i], max);
-            right[i] = max;
-        }
-
-        max = 0;
-        for (int i = 0; i < prices.length; i++) {
-            max = Math.max(left[i] + right[i], max);
-        }
-
+    public int maxPathSum(TreeNode root) {
+        maxPathSumInternal(root);
         return max;
+    }
+
+    private int maxPathSumInternal(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int result = root.val;
+        int leftResult = maxPathSumInternal(root.left);
+        int rightResult = maxPathSumInternal(root.right);
+        max = Math.max(leftResult + rightResult + root.val, max);
+        result = Math.max(result, Math.max(result + leftResult, result + rightResult));
+        max = Math.max(result, max);
+        return result;
     }
 }
