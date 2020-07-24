a871dd493fceec635ea109e6030fee3a4c0d176f
xiakaifa <xkf123456789@gmail.com>
2020-07-25 00:13:13 +0800

    Coding

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,30 +1,20 @@
 package main;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Solution {
-    /**
-     * 每一次都移除栈底元素，然后递归逆序子栈，再将栈底元素压入即可完成逆序
-     */
-    public void reverse(LinkedList<Integer> stack) {
-        if (stack.isEmpty()) {
-            return;
+    public void sortStackByStack(LinkedList<Integer> stack) {
+        LinkedList<Integer> help = new LinkedList<>();
+        while (!stack.isEmpty()) {
+            int currentValue = stack.pop();
+            while (!help.isEmpty() && help.peek() < currentValue) {
+                stack.push(help.pop());
+            }
+            help.push(currentValue);
         }
-        int value = removeAndGet(stack);
-        reverse(stack);
-        stack.push(value);
-    }
-
-    /**
-     * 移除栈底元素并且返回
-     */
-    private int removeAndGet(LinkedList<Integer> stack) {
-        int value = stack.pop();
-        if (stack.isEmpty()) {
-            return value;
+        while (!help.isEmpty()) {
+            stack.push(help.pop());
         }
-        int last = removeAndGet(stack);
-        stack.push(last);
-        return last;
     }
 }
