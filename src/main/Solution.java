dc5c3c2e83892f0bf9d7a5eb74407bfd61ccea22
xiakaifa <xkf123456789@gmail.com>
2020-07-25 22:31:41 +0800

    单调栈结构

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,37 +1,54 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Solution {
     /**
-     * 使用双端队列可以减少时间的损耗，属于用空间换时间的做法
-     * 核心的思想是用双端队列来记录每个区域的值的情况，当窗口向后滑动的时候可以直接得到结果
-     * 使用队列保存值，使得队列保持降序。新的元素入队时直接挤掉队列中小于它的值
-     * 移动窗口时动态记录当前窗口的值，以及删除队头的元素保证窗口的合法性
+     * 单调栈结构。面试中经常会出现的一个问题
+     * 单调栈的核心思想
+     * 如果找到每一个 i 位置左边和右边离 i 位置最近且值比 arr[i]小的位置，那么需要让 stack 从栈顶到栈底的位置所代表的值是严格递减的；
+     * 如果找到每一个 i 位置左边和右边离 i 位置最近且值比 arr[i]大的位置，那么需要让 stack 从栈顶到栈底的位置所代表的值是严格递增的。
      */
-    public int[] getMaxWindow(int[] arr, int w) {
-        // 异常情况直接返回
-        if (arr == null || w < 1 || arr.length < w) {
-            return null;
-        }
-        LinkedList<Integer> queue = new LinkedList<>();
-        int[] res = new int[arr.length - w + 1];
-        int index = 0;
-        // 遍历数组
+    public int[][] getNearLess(int[] arr) {
+        int[][] res = new int[arr.length][2];
+        // 这里栈装的是list是因为考虑到重复值，让所有的重复值放到一个list里面可以更加高效的处理
+        LinkedList<List<Integer>> stack = new LinkedList<>();
+        // 开始循环
         for (int i = 0; i < arr.length; i++) {
-            // 如果当前队列为空，或者当前值小于队尾元素，将当前的坐标放入到队尾
-            while (!queue.isEmpty() && arr[queue.peekLast()] <= arr[i]) {
-                queue.pollLast();
+            // 当前遍历到了一个值，栈顶元素大于他，无法维持顺序，需要逐步弹出
+            while (!stack.isEmpty() && arr[stack.peek().get(0)] > arr[i]) {
+                List<Integer> list = stack.pop();
+                // 找到最左边的小于它元素的下标。其实就是弹出栈顶之后的新栈顶。因为从上到下的递减的，下面的元素一定小于弹出栈顶的值
+                // 如果栈空了，说明这个位置左边是没有比它小的，记为-1
+                int leftLessIndex = stack.isEmpty() ? -1 : stack.peek().get(stack.peek().size() - 1);
+                // 整理结果
+                for (Integer index : list) {
+                    res[index][0] = leftLessIndex;
+                    // 右边第一个小于自己的下标其实就是遍历到的当前这个位置。正是因为自己比它大才会弹出来处理
+                    res[index][1] = i;
+                }
             }
-            // 新的坐标入队
-            queue.addLast(i);
-            // 判读当前的队头元素是不是落在窗口外面了。如果是的话直接删除掉
-            if (queue.peekFirst() == i - w) {
-                queue.pollFirst();
+            // 重复值放到一起
+            if (!stack.isEmpty() && arr[stack.peek().get(0)] == arr[i]) {
+                stack.peek().add(i);
+            } else {
+                // 不是重复值包装成list放进去就可以了
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(i);
+                stack.push(list);
             }
-            // 记录窗口的最大值
-            if (i >= w - 1) {
-                res[index++] = arr[queue.peekFirst()];
+        }
+
+        // 处理栈内剩下的元素
+        // 和之前的处理方式是一样的，只不过是右边不在有比当前位置小的元素的下标了
+        while (!stack.isEmpty()) {
+            List<Integer> list = stack.pop();
+            int leftLessIndex = stack.isEmpty() ? -1 : stack.peek().get(stack.peek().size() - 1);
+            for (Integer index : list) {
+                res[index][0] = leftLessIndex;
+                res[index][1] = -1;
             }
         }
         return res;
