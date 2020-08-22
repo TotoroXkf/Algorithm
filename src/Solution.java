23a2f331cd81c059022260bf586457ac8a667500
xiakaifa <xkf123456789@gmail.com>
2020-08-22 14:08:46 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,25 @@
-import java.util.HashSet;
+import struct.TreeNode;
 
-public class Solution {
+class Solution {
     /**
-     * 遍历数组,当遇到一个数字的时候找他的下一位。然后跌代继续找下下位。动态记录中间的长度即可
-     * 使用HashSet快速的找到下一位，降低复杂度
-     * 中间还可以再度优化，当前这个数字的前一位已经有了，直接跳过当前遍历的这个数字就可以。以为前面肯定被串过了
+     * 相对来说比较直接。
+     * 判断当前的节点是不是叶子节点
+     * 不是叶子节点，将当前节点值*10，递归左右，累加起来返回
+     * 是叶子节点，加上父节点传递下来的值返回
      */
-    public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) {
+    public int sumNumbers(TreeNode root) {
+        return sumNumbers(root, 0);
+    }
+
+    public int sumNumbers(TreeNode root, int parentValue) {
+        if (root == null) {
             return 0;
         }
-        int result = 0;
-        // 装入HashSet
-        HashSet<Integer> hashSet = new HashSet<>();
-        for (int num : nums) {
-            hashSet.add(num);
+        if (root.left == null && root.right == null) {
+            return root.val + parentValue;
         }
-        int currentLen;
-        int currentValue;
-        for (int value : hashSet) {
-            // 有前一位直接跳过
-            if (hashSet.contains(value - 1)) {
-                continue;
-            }
-            // 迭代查找下一位
-            currentValue = value;
-            currentLen = 1;
-            while (hashSet.contains(currentValue + 1)) {
-                currentValue++;
-                currentLen++;
-            }
-            // 动态记录最大值
-            result = Math.max(result, currentLen);
-        }
-        return result;
+        int leftResult = sumNumbers(root.left, (parentValue + root.val) * 10);
+        int rightResult = sumNumbers(root.right, (parentValue + root.val) * 10);
+        return leftResult + rightResult;
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -8,11 +8,6 @@ public class SolutionTest {
 
     @Test
     public void test() {
-        assert 0 == solution.longestConsecutive(testCase.parseToArray("[]"));
-        assert 1 == solution.longestConsecutive(testCase.parseToArray("[1, 1, 1, 1]"));
-        assert 4 == solution.longestConsecutive(testCase.parseToArray("[100, 4, 200, 1, 3, 2]"));
-        assert 2 == solution.longestConsecutive(testCase.parseToArray("[0, -1]"));
-        assert 2 == solution.longestConsecutive(testCase.parseToArray("[0, 0, -1]"));
-        assert 3 == solution.longestConsecutive(testCase.parseToArray("[1, 2, 0, 1]"));
+
     }
 } 
