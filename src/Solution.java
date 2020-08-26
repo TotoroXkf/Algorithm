363b8145cc74abb034a9d42a4a9aea3eaa34f594
xiakaifa <xkf123456789@gmail.com>
2020-08-26 08:43:39 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,59 +1,42 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-public class Solution {
+class Solution {
     /**
-     * 递归法的应用
-     * 抽象的来看这个问题就是先判断从start到i位置的字符串是不是回文字符串，是的话递归后面的字符串，然后从这个位置继续向后找新的回文字符串
-     * 具体的逻辑看代码即可
+     * 这个题很难直接想到动态规划
+     * 演变的过程大约是暴力求解->记忆搜索->动态规划
+     * 所以实际问题还是要迭代优化的好
+     * 思路是这样，dp[i]代表了从0到i需要切几刀
+     * 那么分两个情况
+     * 从0到i都是一个回文串，dp[i] = 0
+     * 从中间某个位置a到i是回文串（也可能只有i自己是回文串），那么从a到i需要1到，之前需要dp[a-1]刀。所以dp[i] = dp[a-1]+1
+     * 每个位置都可能被重复的计算，所以这里还需要做一个最小值的比较dp[i] = min(dp[i],dp[a-1]+1)
+     * 按照这个思路就可以完成了
      */
-    public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        if (s.isEmpty()) {
-            return result;
+    public int minCut(String s) {
+        int[] dp = new int[s.length()];
+        Arrays.fill(dp, s.length() - 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            // 单个字符为中心扩散
+            compute(s, i, i, dp);
+            // 两个字符为中心扩散
+            compute(s, i, i + 1, dp);
         }
-        partition(s, 0, result, new ArrayList<>());
-        return result;
+        return dp[s.length() - 1];
     }
 
-    /**
-     * 主要的处理过程
-     */
-    public void partition(String s, int start, List<List<String>> result, List<String> currentList) {
-        // 完成遍历，直接添加结果
-        if (start >= s.length()) {
-            result.add(new ArrayList<>(currentList));
-            return;
-        }
-
-        for (int i = start; i < s.length(); i++) {
-            // 判断开始到当前位置的字符串是不是回文字符串
-            // 不是回文字符串的话直接跳过，到一下个位置
-            if (!isPalindrome(s, start, i + 1)) {
-                continue;
+    public void compute(String s, int left, int right, int[] dp) {
+        while (left > -1 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            // 说明从0到right整个是个回文字符串，则当前的dp值为0
+            if (left == 0) {
+                dp[right] = 0;
+                return;
             }
-            // 是回文字符串，将当前字符串记录下来
-            currentList.add(s.substring(start, i + 1));
-            // 递归处理后续的字符串
-            partition(s, i + 1, result, currentList);
-            // 当前字符串处理完毕之后移除，往后找新的字符串
-            currentList.remove(currentList.size() - 1);
+            // 从left到right整个是个回文字符串，则从i到j这段只需要切一刀，所以总和应该是 从0到left-1的解 +1
+            // 就是dp[left-1]+1。同时比较出最小的。一个位置可能多次计算
+            dp[right] = Math.min(dp[right], dp[left - 1] + 1);
+            left--;
+            right++;
         }
-    }
-
-    /**
-     * 判断一个字符串是不是回文字符串
-     */
-    public boolean isPalindrome(String s, int left, int right) {
-        int i = left;
-        int j = right - 1;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
     }
 }
