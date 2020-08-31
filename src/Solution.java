d64ea0382d36b4d26f552bcfbf932ba459f4b2b1
xiakaifa <xkf123456789@gmail.com>
2020-08-31 08:39:22 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,22 @@
 class Solution {
     /**
-     * 数组预处理的问题
-     * 首先需要计算出每个位置的消耗值gas[i] - cost[i]
-     * 记录到一个数组里面，同时求和，和小于0直接返回-1
-     * 遍历新生成的数组
-     * 当找到1个正数的时候向后遍历看能不能走到尾，也就是不断地要求叠加大于0
-     * 能的话返回这个正数的下标
-     * 不能的话从中断位置的下一个继续即可
+     * 位运算很多时候确实很难说清楚为什么
+     * 这个题的思路就是，异或操作一次或者3次都会得到数字本身
+     * 那就要区分出3次的情况
+     * 所以设置两个变量 出现一次的标志变量和出现两次的标志变量
+     * 这样两个变量的效果就是
+     * 当数字只出现一次的时候 出现一次的标志变量就是这个数字本身
+     * 当数字只出现三次的时候 出现一次的标志变量和出现两次的标志变量都是0
+     * 但是推到过程实在很难说清楚。。。。
      */
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int[] gains = new int[gas.length];
-        int sum = 0;
-        for (int i = 0; i < gas.length; i++) {
-            gains[i] = gas[i] - cost[i];
-            sum += gains[i];
+    public int singleNumber(int[] nums) {
+        int seenOnce = 0, seenTwice = 0;
+
+        for (int num : nums) {
+            seenOnce = ~seenTwice & (seenOnce ^ num);
+            seenTwice = ~seenOnce & (seenTwice ^ num);
         }
-        if (sum < 0) {
-            return -1;
-        }
-        sum = 0;
-        int i = 0;
-        while (i < gains.length) {
-            sum += gains[i];
-            if (sum < 0) {
-                sum = 0;
-                i++;
-                continue;
-            }
-            int j = i + 1;
-            while (sum >= 0 && j < gains.length) {
-                sum += gains[j];
-                j++;
-            }
-            if (j == gains.length) {
-                return i;
-            }
-            sum = 0;
-            i = j;
-        }
-        return -1;
+
+        return seenOnce;
     }
 }
