045fa12eb9c4779dba580ff7751db67690899bd1
xiakaifa <xkf123456789@gmail.com>
2020-09-20 08:55:37 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,46 @@
 class Solution {
     /**
-     * 比较难以想到的一种算法，也叫投票算法
-     * 首先要想明白一个事情，就是出现次数大于一半，说明存在一个现象，剩下的所有数字和这个出现最多的数字一一相消，最后这个出现最多的数字仍然会剩下至少1个
-     * 所以投票算法就是利用这个思路
-     * 首先指定result为随便一个值，然后当前票数为0
-     * 遍历数组，如果当前票数count为0，则result为当前遍历到的数字
-     * 如果当前数字与result相同，count++,反之减减
-     * 循环结束，result自然就是最后的答案了
-     * 可以生效的原因在于前面说的现象，下的所有数字和这个出现最多的数字一一相消，最后这个出现最多的数字仍然会剩下至少1个
-     * 按照上面的做法其实保证了这个逻辑的成立。就是让数字之间相消。但是不论怎么相消，最后出现多的那个数字都会被剩下来
+     * 这个题的关键在于看到背后的二分本质
+     * 拿到题目之后向判断左右两个边界点是不是满足，满足直接返回
+     * 然后二分中间。如果中间点满足直接返回
+     * 不满足时存在以下情况
+     * 中间值小于左边相邻节点值
+     * 中间值大于左边相邻节点值
+     * 根据题目，相邻节点值不可能相同
+     * 分开讨论。现在的前提条件时nums[1]>nums[0]
+     * <p>
+     * 当中间值小于左边相邻节点值时
+     * 这说明，从0节点向右是上升趋势，而从mid向mid-1是下降趋势
+     * 那么，从0到mid一定会经历一个先升后降的趋势。所以，在0到mid中间存在解。二分左边
+     * <p>
+     * 当中间值大于左边相邻节点值时
+     * 和上面的一样，存在从len-1到len-2是上升趋势
+     * 从mid先mid+1必然也是上升趋势，不然直接就返回mid了
+     * 所以右边势必存在先升后降的趋势。存在解，二分右边
      */
-    public int majorityElement(int[] nums) {
-        int result = nums[0];
-        int count = 0;
-        for (int num : nums) {
-            if (count == 0) {
-                result = num;
+    public int findPeakElement(int[] nums) {
+        if (nums.length < 2 || nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (mid == left || mid == right) {
+                break;
             }
-            if (num == result) {
-                count++;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (nums[mid] < nums[mid - 1]) {
+                right = mid;
             } else {
-                count--;
+                left = mid;
             }
         }
-        return result;
+        return 0;
     }
 }
