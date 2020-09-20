648e8d142c010d85f539052c82a751c7a00a5fe7
xiakaifa <xkf123456789@gmail.com>
2020-09-20 08:13:21 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,26 @@
 class Solution {
     /**
-     * 比较好的分析类型问题
-     * <p>
-     * 首先讨论不存在0的情况
-     * - 全部都是正数，所有数字相乘
-     * - 存在偶数个负数，所有数字相乘
-     * - 存在奇数个负数，从左边往右边，直到最后一个负数的累乘值和从右边往左边直到最后一个负数的累乘值的最大值是答案
-     * 按照上面的情况，可以正向遍历数组，累乘所有结果。再反向遍历数组，累乘所有结果，中间不断比较最大值。这样可以覆盖上面的情况
-     * <p>
-     * 再看存在0的情况
-     * 假设正向遍历。当遇到0时，这一段的解其实已经出来了。要么就是0，要么就是不带0的前面的累乘值
-     * 所以遍历到0，和上面的处理情况一样。乘起来，比较最大值，然后从0的下一个位置开始新的遍历
-     * 从0向后的情况和0之前的情况其实完全没有关系了
+     * 比较难以想到的一种算法，也叫投票算法
+     * 首先要想明白一个事情，就是出现次数大于一半，说明存在一个现象，剩下的所有数字和这个出现最多的数字一一相消，最后这个出现最多的数字仍然会剩下至少1个
+     * 所以投票算法就是利用这个思路
+     * 首先指定result为随便一个值，然后当前票数为0
+     * 遍历数组，如果当前票数count为0，则result为当前遍历到的数字
+     * 如果当前数字与result相同，count++,反之减减
+     * 循环结束，result自然就是最后的答案了
+     * 可以生效的原因在于前面说的现象，下的所有数字和这个出现最多的数字一一相消，最后这个出现最多的数字仍然会剩下至少1个
+     * 按照上面的做法其实保证了这个逻辑的成立。就是让数字之间相消。但是不论怎么相消，最后出现多的那个数字都会被剩下来
      */
-    public int maxProduct(int[] nums) {
-        int currentValue = 1;
+    public int majorityElement(int[] nums) {
         int result = nums[0];
+        int count = 0;
         for (int num : nums) {
-            currentValue *= num;
-            result = Math.max(result, currentValue);
-            if (num == 0) {
-                currentValue = 1;
+            if (count == 0) {
+                result = num;
             }
-        }
-        currentValue = 1;
-        for (int i = nums.length - 1; i > -1; i--) {
-            currentValue *= nums[i];
-            result = Math.max(result, currentValue);
-            if (nums[i] == 0) {
-                currentValue = 1;
+            if (num == result) {
+                count++;
+            } else {
+                count--;
             }
         }
         return result;
---
+++ src/SolutionTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    Solution solution = new Solution();
+
+    @Test
+    public void findMin() {
+
+    }
+}
