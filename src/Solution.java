a7afa4e5ebb525046c910dcabbf3ff562d76b68f
xiakaifa <xkf123456789@gmail.com>
2020-09-22 09:26:13 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,54 +1,48 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     /**
-     * 使用分桶的思想解决的问题，算是很巧妙的算法问题了
-     * 将所有数字分到 k 个桶当中
-     * 按照数学上面的分析，最大的间距一定存在桶与桶之间，而不可能存在桶的内部
-     * 所以算法就是分桶，然后遍历桶，桶之间做差，求出最大值
+     * 算是个数学问题把。。。。。
+     * 具体的解法看笔记上的题解图吧
      */
-    public int maximumGap(int[] nums) {
-        if (nums.length < 2) {
-            return 0;
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
         }
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int num : nums) {
-            max = Math.max(max, num);
-            min = Math.min(min, num);
+        StringBuilder result = new StringBuilder();
+        // 出现负数，记录负号
+        if ((numerator > 0 && denominator < 0) || (numerator < 0 && denominator > 0)) {
+            result.append("-");
         }
-        // 求桶之间的间距
-        int bucketGap = (int) Math.ceil((double) (max - min) / (nums.length - 1));
-        // 没有间距说明全都相同，直接返回0
-        if (bucketGap == 0) {
-            return 0;
+        // 使用正数来计算结果
+        long above = Math.abs((long) numerator);
+        long below = Math.abs((long) denominator);
+        long value = above % below;
+        // 可以直接整除的情况直接返回
+        if (value == 0) {
+            return result.append(above / below).toString();
         }
-        // 计算桶的个数。其实从数学上来说桶的个数就是数组长度-1，但是计算机计算的问题，所以这里计算完+1保证桶是足够的
-        int bucketNum = (max - min) / bucketGap + 1;
-        // 记录每个桶的最大值的数组
-        int[] bucketMax = new int[bucketNum];
-        // 记录每个桶的最小值的数组
-        int[] bucketMin = new int[bucketNum];
-        Arrays.fill(bucketMax, Integer.MIN_VALUE);
-        Arrays.fill(bucketMin, Integer.MAX_VALUE);
-        // 更新每个桶的最大最小值
-        for (int num : nums) {
-            int index = (num - min) / bucketGap;
-            bucketMax[index] = Math.max(bucketMax[index], num);
-            bucketMin[index] = Math.min(bucketMin[index], num);
-        }
-        int result = bucketGap;
-        int lastMax = min;
-        for (int i = 0; i < bucketNum; i++) {
-            // 这种情况说明桶里面没东西，这个桶跳过
-            if (bucketMax[i] == Integer.MIN_VALUE && bucketMin[i] == Integer.MAX_VALUE) {
-                continue;
+        HashMap<Long, Integer> hashMap = new HashMap<>();
+        // 首位
+        result.append(Math.abs((long) numerator) / Math.abs((long) denominator));
+        result.append(".");
+        while (value != 0) {
+            // 关键的算法，把余数*10
+            value *= 10;
+            // 如果出现过，说明会出现循环小数，在出现的位置叠加（）
+            if (hashMap.containsKey(value)) {
+                int index = hashMap.get(value);
+                result.insert(index, "(");
+                result.append(")");
+                break;
             }
-            // 更新最大值，当前桶的最小值减去上一个桶的最大值。可能是跨过多个桶的
-            result = Math.max(bucketMin[i] - lastMax, result);
-            // 更新上一个桶的最大值
-            lastMax = bucketMax[i];
+            // 记录出现过的余数
+            hashMap.put(value, result.length());
+            // 当前位的数字
+            result.append(value / below);
+            // 计算下一个位置的值
+            value %= below;
         }
-        return result;
+        return result.toString();
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -7,6 +7,10 @@ public class SolutionTest {
 
     @Test
     public void findMin() {
-
+        assert "2".equals(solution.fractionToDecimal(2, 1));
+        assert "0.5".equals(solution.fractionToDecimal(1, 2));
+        assert "0.(6)".equals(solution.fractionToDecimal(2, 3));
+        assert "0.1(6)".equals(solution.fractionToDecimal(1, 6));
+        assert "0.(4)".equals(solution.fractionToDecimal(4, 9));
     }
 }
