bc9fa412ab586f8fed3fce0e9a6e352ccb427ad1
xiakaifa <xkf123456789@gmail.com>
2020-09-27 09:32:00 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,23 @@
-import java.math.BigDecimal;
+import java.util.Arrays;
 
 class Solution {
     public String largestNumber(int[] nums) {
-        StringBuilder current = new StringBuilder();
-        StringBuilder result = new StringBuilder("0");
-        largestNumber(nums, 0, current, result, new boolean[nums.length]);
-        return result.toString();
-    }
-
-    private void largestNumber(int[] nums, int index, StringBuilder current, StringBuilder result, boolean[] marked) {
-        if (index >= nums.length) {
-            if (new BigDecimal(current.toString()).compareTo(new BigDecimal(result.toString())) > 0) {
-                result.delete(0, result.length());
-                result.append(current.toString());
-                return;
-            }
-        }
+        String[] numStrings = new String[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            if (marked[i]) {
-                continue;
-            }
-            String s = String.valueOf(nums[i]);
-            current.append(s);
-            index++;
-            marked[i] = true;
-            largestNumber(nums, index, current, result, marked);
-            current.delete(current.length() - s.length(), current.length());
-            index--;
-            marked[i] = false;
+            numStrings[i] = String.valueOf(nums[i]);
         }
+        Arrays.sort(numStrings, (str1, str2) -> {
+            String num1 = str1 + str2;
+            String num2 = str2 + str1;
+            return num2.compareTo(num1);
+        });
+        if (numStrings[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String numString : numStrings) {
+            stringBuilder.append(numString);
+        }
+        return stringBuilder.toString();
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -14,4 +14,34 @@ public class SolutionTest {
         int[] array = new int[]{3, 30, 34, 5, 9};
         assert "9534330".equals(solution.largestNumber(array));
     }
+
+    @Test
+    public void check2() {
+        int[] array = new int[]{1, 4, 7, 2, 5, 8, 0, 3, 6, 9};
+        assert "9876543210".equals(solution.largestNumber(array));
+    }
+
+    @Test
+    public void check3() {
+        int[] array = new int[]{121, 12};
+        assert "12121".equals(solution.largestNumber(array));
+    }
+
+    @Test
+    public void check4() {
+        int[] array = new int[]{1212, 12};
+        assert "121212".equals(solution.largestNumber(array));
+    }
+
+    @Test
+    public void check5() {
+        int[] array = new int[]{1212, 1212};
+        assert "12121212".equals(solution.largestNumber(array));
+    }
+
+    @Test
+    public void check6() {
+        int[] array = new int[]{0, 0};
+        assert "0".equals(solution.largestNumber(array));
+    }
 }
