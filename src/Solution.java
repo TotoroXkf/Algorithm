dea4b1d3df94b65beac094dee7f230f2c24f3e0c
xiakaifa <xkf123456789@gmail.com>
2020-10-04 14:57:54 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,39 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 class Solution {
-    public String largestNumber(int[] nums) {
-        String[] numStrings = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            numStrings[i] = String.valueOf(nums[i]);
+    public List<String> findRepeatedDnaSequences(String s) {
+        int len = 10;
+        if (s.length() < len) {
+            return new ArrayList<>();
         }
-        Arrays.sort(numStrings, (str1, str2) -> {
-            String num1 = str1 + str2;
-            String num2 = str2 + str1;
-            return num2.compareTo(num1);
-        });
-        if (numStrings[0].equals("0")) {
-            return "0";
+        int base = 4;
+        int factorial = (int) Math.pow(base, len);
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        hashMap.put('A', 0);
+        hashMap.put('C', 1);
+        hashMap.put('G', 2);
+        hashMap.put('T', 3);
+        int[] num = new int[s.length()];
+        int value = 0;
+        for (int i = 0; i < s.length(); i++) {
+            num[i] = hashMap.get(s.charAt(i));
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String numString : numStrings) {
-            stringBuilder.append(numString);
+        for (int i = 0; i < len; i++) {
+            value = value * base + num[i];
         }
-        return stringBuilder.toString();
+        HashSet<Integer> repeatSet = new HashSet<>();
+        repeatSet.add(value);
+        HashSet<String> resultSet = new HashSet<>();
+        int start = 0;
+        for (int i = len; i < s.length(); i++, start++) {
+            value = value * base + num[i] - num[start] * factorial;
+            if (!repeatSet.add(value)) {
+                resultSet.add(s.substring(i + 1 - len, i + 1));
+            }
+        }
+        return new ArrayList<>(resultSet);
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -1,47 +1,16 @@
 import org.junit.Test;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class SolutionTest {
     Solution solution = new Solution();
 
     @Test
     public void check() {
-        int[] array = new int[]{10, 2};
-        assert "210".equals(solution.largestNumber(array));
-    }
-
-    @Test
-    public void check1() {
-        int[] array = new int[]{3, 30, 34, 5, 9};
-        assert "9534330".equals(solution.largestNumber(array));
-    }
-
-    @Test
-    public void check2() {
-        int[] array = new int[]{1, 4, 7, 2, 5, 8, 0, 3, 6, 9};
-        assert "9876543210".equals(solution.largestNumber(array));
-    }
-
-    @Test
-    public void check3() {
-        int[] array = new int[]{121, 12};
-        assert "12121".equals(solution.largestNumber(array));
-    }
-
-    @Test
-    public void check4() {
-        int[] array = new int[]{1212, 12};
-        assert "121212".equals(solution.largestNumber(array));
-    }
-
-    @Test
-    public void check5() {
-        int[] array = new int[]{1212, 1212};
-        assert "12121212".equals(solution.largestNumber(array));
-    }
-
-    @Test
-    public void check6() {
-        int[] array = new int[]{0, 0};
-        assert "0".equals(solution.largestNumber(array));
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        List<String> result = solution.findRepeatedDnaSequences(s);
+        assert result.get(0).equals("AAAAACCCCC");
+        assert result.get(1).equals("CCCCCAAAAA");
     }
 }
