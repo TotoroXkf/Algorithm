648a485e035939ede5636637494f9a7c1b81493a
xiakaifa <xkf123456789@gmail.com>
2020-10-12 08:47:32 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,21 @@
 class Solution {
-    public int rangeBitwiseAnd(int m, int n) {
-        while (m < n) {
-            n = n & (n - 1);
+    public boolean isHappy(int n) {
+        int fast = getNext(n);
+        int slow = n;
+        while (fast != 1 && fast != slow) {
+            fast = getNext(getNext(fast));
+            slow = getNext(slow);
         }
-        return n;
+        return fast == 1;
+    }
+
+    private int getNext(int n) {
+        int result = 0;
+        while (n > 0) {
+            int num = n % 10;
+            result += num * num;
+            n /= 10;
+        }
+        return result;
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -2,25 +2,4 @@ import org.junit.Test;
 
 public class SolutionTest {
     Solution solution = new Solution();
-
-    @Test
-    public void check() {
-        int m = 0;
-        int n = 1;
-        assert 0 == solution.rangeBitwiseAnd(m, n);
-    }
-
-    @Test
-    public void check2() {
-        int m = 2;
-        int n = 3;
-        assert 2 == solution.rangeBitwiseAnd(m, n);
-    }
-
-    @Test
-    public void check3() {
-        int m = 14;
-        int n = 19;
-        assert 0 == solution.rangeBitwiseAnd(m, n);
-    }
 }
