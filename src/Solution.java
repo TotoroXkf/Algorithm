aff2f350d077e4c3d1b9ed9846898d026aac4ed1
xiakaifa <xkf123456789@gmail.com>
2020-10-12 08:25:22 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,8 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        if (grid.length == 0 || grid[0].length == 0) {
-            return 0;
+    public int rangeBitwiseAnd(int m, int n) {
+        while (m < n) {
+            n = n & (n - 1);
         }
-        boolean[][] mark = new boolean[grid.length][grid[0].length];
-        int result = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1' && !mark[i][j]) {
-                    result++;
-                    dfs(grid, mark, i, j);
-                }
-            }
-        }
-        return result;
-    }
-
-    private void dfs(char[][] grid, boolean[][] mark, int row, int column) {
-        if (row < 0 || row >= grid.length || column < 0 || column >= grid[0].length || mark[row][column] || grid[row][column] == '0') {
-            return;
-        }
-        mark[row][column] = true;
-        dfs(grid, mark, row - 1, column);
-        dfs(grid, mark, row + 1, column);
-        dfs(grid, mark, row, column - 1);
-        dfs(grid, mark, row, column + 1);
+        return n;
     }
 }
