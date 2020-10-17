e8cfd953f23bd47033ffdcb8a87668c9ae2739ff
xiakaifa <xkf123456789@gmail.com>
2020-10-17 09:12:27 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,51 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
-    public int minSubArrayLen(int s, int[] nums) {
-        int currentSum = 0;
-        int left = 0;
-        int right = 0;
-        int result = 0;
-        while (right < nums.length) {
-            currentSum += nums[right];
-            while (currentSum >= s && left <= right) {
-                if (result == 0) {
-                    result = right - left + 1;
-                } else {
-                    result = Math.min(right - left + 1, result);
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> edges = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+        for (int[] ints : prerequisites) {
+            edges.get(ints[1]).add(ints[0]);
+        }
+        // 状态表，0表示从未被搜索过，1表示搜索中，2表示已经完成搜索
+        int[] state = new int[numCourses];
+        LinkedList<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) {
+                boolean result = dfs(state, edges, i, stack);
+                if (!result) {
+                    return new int[0];
                 }
-                currentSum -= nums[left];
-                left++;
             }
-            right++;
+        }
+        int[] result = new int[stack.size()];
+        int i = 0;
+        while (!stack.isEmpty()) {
+            result[i++] = stack.pop();
         }
         return result;
+    }
+
+    private boolean dfs(int[] state, List<List<Integer>> edges, int node, LinkedList<Integer> stack) {
+        if (state[node] == 1) {
+            return false;
+        }
+        if (state[node] == 2) {
+            return true;
+        }
+        state[node] = 1;
+        for (int adjacentNode : edges.get(node)) {
+            boolean result = dfs(state, edges, adjacentNode, stack);
+            if (!result) {
+                return false;
+            }
+        }
+        state[node] = 2;
+        stack.push(node);
+        return true;
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -5,23 +5,17 @@ public class SolutionTest {
 
     @Test
     public void check() {
-        int[] array = new int[]{2, 3, 1, 2, 4, 4};
-        int target = 7;
-        assert 2 == solution.minSubArrayLen(target, array);
+
     }
 
     @Test
     public void check1() {
-        int[] array = new int[]{10, 3, 1, 2, 4, 4};
-        int target = 7;
-        assert 1 == solution.minSubArrayLen(target, array);
+
     }
 
     @Test
     public void check2() {
-        int[] array = new int[]{1, 1};
-        int target = 3;
-        assert 0 == solution.minSubArrayLen(target, array);
+
     }
 
     @Test
