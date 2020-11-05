04c90f3b59ba30f28fd7b3f242dc11e1a64cd54f
xiakaifa <xkf123456789@gmail.com>
2020-11-05 09:08:29 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,35 @@
-import java.util.HashMap;
-
-class Solution {
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        HashMap<Long, Long> hashMap = new HashMap<>();
-        long w = (long) t + 1;
-        for (int i = 0; i < nums.length; i++) {
-            long id = getId(nums[i], w);
-            if (hashMap.containsKey(id)) {
-                return true;
-            }
-            if (hashMap.containsKey(id - 1) && (long) nums[i] - hashMap.get(id - 1) <= t) {
-                return true;
-            }
-            if (hashMap.containsKey(id + 1) && hashMap.get(id + 1) - (long) nums[i] <= t) {
-                return true;
-            }
-            hashMap.put(id, (long) nums[i]);
-            if (hashMap.size() > k) {
-                hashMap.remove(getId(nums[i - k], w));
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int result = 0;
+        int[] dp = new int[matrix[0].length];
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == '1') {
+                dp[i] = 1;
+                result = 1;
             }
         }
-        return false;
-    }
-
-    private long getId(long x, long w) {
-        return x < 0 ? ((x + 1) / w) - 1 : x / w;
+        for (int i = 1; i < matrix.length; i++) {
+            int leftTopValue = dp[0];
+            dp[0] = 0;
+            if (matrix[i][0] == '1') {
+                dp[0] = 1;
+                result = Math.max(result, 1);
+            }
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '0') {
+                    leftTopValue = dp[j];
+                    dp[j] = 0;
+                    continue;
+                }
+                int temp = dp[j];
+                dp[j] = Math.min(leftTopValue, Math.min(dp[j - 1], dp[j])) + 1;
+                leftTopValue = temp;
+                result = Math.max(result, dp[j] * dp[j]);
+            }
+        }
+        return result;
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -5,34 +5,39 @@ public class SolutionTest {
 
     @Test
     public void check() {
-        int[] nums = new int[]{1, 2, 3, 1};
-        int k = 3;
-        int t = 0;
-        assert solution.containsNearbyAlmostDuplicate(nums, k, t);
+        char[][] nums = new char[][]{
+            new char[]{'1', '0', '1', '0', '0'},
+            new char[]{'1', '0', '1', '1', '1'},
+            new char[]{'1', '1', '1', '1', '1'},
+            new char[]{'1', '0', '0', '1', '0'}
+        };
+        assert solution.maximalSquare(nums) == 4;
     }
 
     @Test
     public void check1() {
-        int[] nums = new int[]{1, 0, 1, 1};
-        int k = 1;
-        int t = 2;
-        assert solution.containsNearbyAlmostDuplicate(nums, k, t);
+        char[][] nums = new char[][]{
+            new char[]{'0', '1'},
+            new char[]{'0', '1'}
+        };
+        assert solution.maximalSquare(nums) == 1;
     }
 
     @Test
     public void check2() {
-        int[] nums = new int[]{1, 5, 9, 1, 5, 9};
-        int k = 2;
-        int t = 3;
-        assert !solution.containsNearbyAlmostDuplicate(nums, k, t);
+        char[][] nums = new char[][]{
+            new char[]{'1', '1', '1', '1', '0'},
+            new char[]{'1', '1', '1', '1', '0'},
+            new char[]{'1', '1', '1', '1', '1'},
+            new char[]{'1', '1', '1', '1', '1'},
+            new char[]{'0', '0', '1', '1', '1'}
+        };
+        assert solution.maximalSquare(nums) == 16;
     }
 
     @Test
     public void check3() {
-        int[] nums = new int[]{-2147483648,2147483647};
-        int k = 1;
-        int t = 1;
-        assert !solution.containsNearbyAlmostDuplicate(nums, k, t);
+
     }
 
     @Test
@@ -62,11 +67,6 @@ public class SolutionTest {
 
     @Test
     public void check9() {
-
-    }
-
-    @Test
-    public void check10() {
 
     }
 }
