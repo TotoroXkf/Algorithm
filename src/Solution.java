f46b8c9330ffc7e9528fc719ba16cbbffe143d97
xiakaifa <xkf123456789@gmail.com>
2020-11-06 09:22:01 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,10 @@
-public class Solution {
-    public int maximalSquare(char[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
-        }
-        int result = 0;
-        int[] dp = new int[matrix[0].length];
-        for (int i = 0; i < matrix[0].length; i++) {
-            if (matrix[0][i] == '1') {
-                dp[i] = 1;
-                result = 1;
-            }
-        }
-        for (int i = 1; i < matrix.length; i++) {
-            int leftTopValue = dp[0];
-            dp[0] = 0;
-            if (matrix[i][0] == '1') {
-                dp[0] = 1;
-                result = Math.max(result, 1);
-            }
-            for (int j = 1; j < matrix[0].length; j++) {
-                if (matrix[i][j] == '0') {
-                    leftTopValue = dp[j];
-                    dp[j] = 0;
-                    continue;
-                }
-                int temp = dp[j];
-                dp[j] = Math.min(leftTopValue, Math.min(dp[j - 1], dp[j])) + 1;
-                leftTopValue = temp;
-                result = Math.max(result, dp[j] * dp[j]);
-            }
-        }
-        return result;
+class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        long X = Math.max(0, (long) Math.min(C, G) - (long) Math.max(A, E));
+        long Y = Math.max(0, (long) Math.min(D, H) - (long) Math.max(B, F));
+        long area1 = ((long) C - (long) A) * ((long) D - (long) B);
+        long area2 = ((long) G - (long) E) * ((long) H - (long) F);
+        long area3 = X * Y;
+        return (int) (area1 + area2 - area3);
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -5,34 +5,17 @@ public class SolutionTest {
 
     @Test
     public void check() {
-        char[][] nums = new char[][]{
-            new char[]{'1', '0', '1', '0', '0'},
-            new char[]{'1', '0', '1', '1', '1'},
-            new char[]{'1', '1', '1', '1', '1'},
-            new char[]{'1', '0', '0', '1', '0'}
-        };
-        assert solution.maximalSquare(nums) == 4;
+
     }
 
     @Test
     public void check1() {
-        char[][] nums = new char[][]{
-            new char[]{'0', '1'},
-            new char[]{'0', '1'}
-        };
-        assert solution.maximalSquare(nums) == 1;
+
     }
 
     @Test
     public void check2() {
-        char[][] nums = new char[][]{
-            new char[]{'1', '1', '1', '1', '0'},
-            new char[]{'1', '1', '1', '1', '0'},
-            new char[]{'1', '1', '1', '1', '1'},
-            new char[]{'1', '1', '1', '1', '1'},
-            new char[]{'0', '0', '1', '1', '1'}
-        };
-        assert solution.maximalSquare(nums) == 16;
+
     }
 
     @Test
