fca870a9726250d9b1ae8fdb72895ce3a685abc8
xiakaifa <xkf123456789@gmail.com>
2020-11-10 08:22:58 +0800

    Coding

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,65 @@
-class Solution {
-    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        long X = Math.max(0, (long) Math.min(C, G) - (long) Math.max(A, E));
-        long Y = Math.max(0, (long) Math.min(D, H) - (long) Math.max(B, F));
-        long area1 = ((long) C - (long) A) * ((long) D - (long) B);
-        long area2 = ((long) G - (long) E) * ((long) H - (long) F);
-        long area3 = X * Y;
-        return (int) (area1 + area2 - area3);
+import java.util.LinkedList;
+
+public class Solution {
+    public int calculate(String s) {
+        LinkedList<Integer> numberStack = new LinkedList<>();
+        LinkedList<Character> operatorStack = new LinkedList<>();
+        int index = 0;
+        while (index < s.length()) {
+            if (s.charAt(index) == ' ') {
+                index++;
+                continue;
+            }
+            if (!Character.isDigit(s.charAt(index))) {
+                int level = getLevel(s.charAt(index));
+                while (!operatorStack.isEmpty() && level <= getLevel(operatorStack.peek())) {
+                    compute(numberStack, operatorStack);
+                }
+                operatorStack.push(s.charAt(index++));
+                continue;
+            }
+            int start = index;
+            while (index < s.length() && Character.isDigit(s.charAt(index))) {
+                index++;
+            }
+            numberStack.push(Integer.parseInt(s.substring(start, index)));
+        }
+        while (!operatorStack.isEmpty()) {
+            compute(numberStack, operatorStack);
+        }
+        return numberStack.pop();
+    }
+
+    private void compute(LinkedList<Integer> numberStack, LinkedList<Character> operatorStack) {
+        char operator = operatorStack.pop();
+        int number1 = numberStack.pop();
+        int number2 = numberStack.pop();
+        switch (operator) {
+            case '+':
+                numberStack.push(number1 + number2);
+                break;
+            case '-':
+                numberStack.push(number2 - number1);
+                break;
+            case '*':
+                numberStack.push(number1 * number2);
+                break;
+            case '/':
+                numberStack.push(number2 / number1);
+                break;
+            default:
+        }
+    }
+
+    private int getLevel(char operator) {
+        switch (operator) {
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return 0;
+        }
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -5,27 +5,32 @@ public class SolutionTest {
 
     @Test
     public void check() {
-
+        String str = "3+2*2";
+        assert 7 == solution.calculate(str);
     }
 
     @Test
     public void check1() {
-
+        String str = "3/2";
+        assert 1 == solution.calculate(str);
     }
 
     @Test
     public void check2() {
-
+        String str = " 33+5 / 2 ";
+        assert 35 == solution.calculate(str);
     }
 
     @Test
     public void check3() {
-
+        String str = "1-1+1";
+        assert 1 == solution.calculate(str);
     }
 
     @Test
     public void check4() {
-
+        String str = "1-1-1";
+        assert -1 == solution.calculate(str);
     }
 
     @Test
