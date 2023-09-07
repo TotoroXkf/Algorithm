12b0fce1fafe7fa393e4de294bf0623503df5f8f
xiakaifa <xiakaifa@bytedance.com>
2023-09-07 10:08:56 +0800

    LeetCode 205

--- Solution.java
+++ Solution.java
@@ -1,5 +1,35 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    private Map<Character, Character> map = new HashMap<>();
+    private Set<Character> set = new HashSet<>();
+
+    public boolean isIsomorphic(String s, String t) {
+        return recursive(s, t, 0);
+    }
+
+    private boolean recursive(String s, String t, int index) {
+        if (index >= s.length() || index >= t.length()) {
+            return true;
+        }
+        if (map.containsKey(s.charAt(index))) {
+            if (map.get(s.charAt(index)) == t.charAt(index)) {
+                return recursive(s, t, index + 1);
+            } else {
+                return false;
+            }
+        } else {
+            if (set.contains(t.charAt(index))) {
+                return false;
+            } else {
+                set.add(t.charAt(index));
+                map.put(s.charAt(index), t.charAt(index));
+                return recursive(s, t, index + 1);
+            }
+
+        }
     }
 }
