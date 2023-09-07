cd628adf147b533c507bb3657a4a5094e803faeb
xiakaifa <xiakaifa@bytedance.com>
2023-09-07 19:27:12 +0800

    LeetCode 206

--- Solution.java
+++ Solution.java
@@ -1,35 +1,17 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class Solution {
-    private Map<Character, Character> map = new HashMap<>();
-    private Set<Character> set = new HashSet<>();
-
-    public boolean isIsomorphic(String s, String t) {
-        return recursive(s, t, 0);
-    }
-
-    private boolean recursive(String s, String t, int index) {
-        if (index >= s.length() || index >= t.length()) {
-            return true;
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
         }
-        if (map.containsKey(s.charAt(index))) {
-            if (map.get(s.charAt(index)) == t.charAt(index)) {
-                return recursive(s, t, index + 1);
-            } else {
-                return false;
-            }
-        } else {
-            if (set.contains(t.charAt(index))) {
-                return false;
-            } else {
-                set.add(t.charAt(index));
-                map.put(s.charAt(index), t.charAt(index));
-                return recursive(s, t, index + 1);
-            }
-
+        ListNode leftNode = head;
+        ListNode rightNode = head.next;
+        while (rightNode != null) {
+            ListNode nextNode = rightNode.next;
+            rightNode.next = leftNode;
+            leftNode = rightNode;
+            rightNode = nextNode;
         }
+        head.next = null;
+        return leftNode;
     }
 }
