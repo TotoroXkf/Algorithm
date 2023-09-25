76184c52692326b1ba24dd4cd56d7be84b3bc01e
xiakaifa <xiakaifa@bytedance.com>
2023-09-25 15:56:56 +0800

    code

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,49 @@
-public class Solution {
-    public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return findKthLargest(nums, k, 0, nums.length);
     }
 
-    private int rob(int[] nums, int startIndex, int endIndex) {
-        int subAnswer1 = 0;
-        int subAnswer2 = nums[startIndex];
-        for (int i = startIndex + 1; i < endIndex + 1; i++) {
-            int newAnswer = Math.max(subAnswer2, nums[i] + subAnswer1);
-            subAnswer1 = subAnswer2;
-            subAnswer2 = newAnswer;
+    private int findKthLargest(int[] nums, int k, int start, int len) {
+        int[] adjustResult = adjust(nums, start, len, nums[start]);
+        int lessEnd = adjustResult[0];
+        int greaterStart = adjustResult[1];
+        int greaterNum = start + len - greaterStart;
+        int lessNum = lessEnd - start;
+        if (greaterNum >= k) {
+            return findKthLargest(nums, k, greaterStart, start + len - greaterStart);
+        } else if (k > len - lessNum) {
+            return findKthLargest(nums, k - (len - lessNum), start, lessEnd - start);
+        } else {
+            return nums[lessEnd];
         }
-        return subAnswer2;
+    }
+
+    private int[] adjust(int[] nums, int start, int len, int target) {
+        int lessEnd = start;
+        int greaterStart = start + len;
+        int index = start;
+        while (index < greaterStart) {
+            if (nums[index] == target) {
+                index++;
+            } else if (nums[index] > target) {
+                greaterStart--;
+                swap(nums, index, greaterStart);
+            } else {
+                swap(nums, index, lessEnd);
+                lessEnd++;
+                index++;
+            }
+        }
+        return new int[]{lessEnd, greaterStart};
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
     }
 }
